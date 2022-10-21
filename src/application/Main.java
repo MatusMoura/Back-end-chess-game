2e13e5fc78731aff591631f7fe2d449a7a588c40
MatusMoura <matus.barreto@gmial.com>
2022-10-21 13:34:05 -0300

    implementando logica do tabuleiro

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,15 @@
 package application;
 
+import board.game.Board;
+import board.game.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
 
+		Board bord = new Board(8, 8);
+
+		System.out.println(bord);
 	}
 
 }
