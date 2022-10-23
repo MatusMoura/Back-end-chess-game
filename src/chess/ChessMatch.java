d804ebfeaf521281e467bde8d846faa77f56ff79
MatusMoura <matus.barreto@gmial.com>
2022-10-23 19:40:20 -0300

    implementando cor as pecas

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import board.game.Board;
-import board.game.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -29,13 +28,18 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(board,Color.WHITE));
-		placeNewPiece('f', 3, new Rook(board,Color.WHITE));
-		placeNewPiece('e', 1, new King(board, Color.BLACK));
-		placeNewPiece('c', 5, new King(board, Color.BLACK));
-		placeNewPiece('a', 6, new Rook(board,Color.BLACK));
-		placeNewPiece('f', 2, new Rook(board,Color.BLACK));
-		placeNewPiece('e', 5, new King(board, Color.WHITE));
-		placeNewPiece('c', 7, new King(board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+		
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 }
